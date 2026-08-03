29e1ab0a9d0e1cddc77ff8e9817a12fc06f9770b
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-08-03 16:49:05 +0300

    Add the DataLoader class and load method

--- src/automl/data_loader.py
+++ src/automl/data_loader.py
@@ -1,2 +1,35 @@
 # Here we will implement the dataLoader
 
+import pandas as pd
+
+
+# Using a class instead of standalone functions lets the loader store configuration
+# (like the dataset path and target column) and makes it easy to extend later.
+class DataLoader:
+    
+    def __init__(self, path, target):
+
+        self.path = path # dataset path
+        self.target = target # target column that we want to predict 
+
+
+    # The main method of the class
+
+    def load(self):
+        df = pd.read_csv(self.path) # read the CSV
+
+        # Get the inputs (x)
+        
+        X = df.drop(columns = [self.target]) # Get all the columns except the trager 
+
+        y = df[self.target] # Get the target / to be predicted column from the data frame
+
+
+        # Instead of asking the user: is this problem classification or regression 
+
+        # Detect what the task should be 
+        task = self.detect_task(y)
+
+        return X, y, task
+
+
