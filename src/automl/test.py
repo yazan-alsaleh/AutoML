d5faf79441c5fc9041f882466affbaec57149cdb
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-08-04 15:29:12 +0300

    Add validator.py to validate the data before preprocessing and testing the models

--- src/automl/test.py
+++ src/automl/test.py
@@ -1,15 +1,28 @@
-from automl.data_loader import DataLoader
-from automl.preprocessor import Preprocessor
+# from automl.data_loader import DataLoader
+# from automl.preprocessor import Preprocessor
 
-loader = DataLoader(path=r"src\automl\Salary_dataset.csv", target="Salary")
+# loader = DataLoader(path=r"data\Salary_Regression.csv", target="Salary")
 
-X, y, task = loader.load()
+# X, y, task = loader.load()
 
-processor = Preprocessor()
+# processor = Preprocessor()
 
-pipeline = processor.build(X)
+# pipeline = processor.build(X)
 
-X_processed = pipeline.fit_transform(X)
+# X_processed = pipeline.fit_transform(X)
 
-print(X_processed.shape)
+# print(X_processed.shape)
+
+import pandas as pd
+from automl.validator import Validator
+
+df = pd.DataFrame({
+     "age": [20, 30, 40],
+    "salary": [3000, 5000, 7000],
+    "target": [0, 1, 1]
+})
+
+validator = Validator(df, target="target")
+
+validator.validate()
 
---
+++ src/automl/validator.py
@@ -0,0 +1,53 @@
+# this file will check if the data is usable before we spend time preprocessing or training models.
+
+
+class Validator:
+
+    def __init__(self, df, target):
+
+        self.df = df
+        self.target = target
+
+
+    # This will be the main method that will have all other methods inside it for validation
+    # When the user call validator.validate all validation methods will be called automatically 
+    def validate(self):
+
+        self.check_empty()
+        self.check_target_exists()
+        self.check_missing_target()
+        self.check_columns()
+
+        print("Validation passed!")
+
+
+    def check_empty(self):
+
+        if self.df.empty:
+            raise ValueError("Dataset is empty.")
+
+
+
+    def check_target_exists(self):
+
+        if self.target not in self.df.columns:
+            raise ValueError(f"Target column {self.target} does not exist.")
+
+
+
+    # if there any missing values in the target column
+    def check_missing_target(self):
+
+        missing = self.df[self.target].isna().sum()
+
+        if missing > 0:
+            raise ValueError("Target column contains missing values.")
+
+
+
+    def check_columns(self):
+
+        if len(self.df.columns) < 2:
+            raise ValueError("Dataset needs at least one feature and one target column.")
+
+
