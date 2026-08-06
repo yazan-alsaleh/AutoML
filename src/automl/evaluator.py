fc599d74a68d1ea6d82fc93ae9fc027c17d5ad1d
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-08-06 17:07:40 +0300

    Finish evaluator file

---
+++ src/automl/evaluator.py
@@ -0,0 +1,63 @@
+# the part that actually makes your system "AutoML". it test all models and return the best one
+
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
+
+
+class Evaluator:
+
+    def __init__(self, task):
+
+        self.task = task
+
+        self.results = {}
+
+        self.best_model = None
+        self.best_score = None
+
+
+    def evaluate(self, models, X_train, X_test, y_train, y_test):
+
+        for name, model in model.items(): # model comes from model zoo
+                                # item will give both model name and model object
+
+            print(f"Training {name}...")
+
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+
+            # Predict
+            predictions = model.predict(X_test)
+
+            # Get the score of the model on testing data
+
+            if self.task == "classification":
+
+                score = accuracy_score(y_test, predictions)
+
+            else:
+
+                score = accuracy_score(y_test, predictions)
+
+
+            # Save the score of each model to show them later in the final report 
+            self.results[name] = score
+
+
+        self._find_best_model()
+
+        return self.results
+
+
+
+
+    def _find_best_model(self):
+
+        self.best_model = max(self.results, key = self.results.get)
+        # max by default compares keys (the models names), we want to compare values, so use (key) argument, to compare the values not the keys of the dictionary.
+        # max here is only to compare, it will store the key (the model name).
+
+        self.best_score = self.results[self.best_model]
+        # Go inside the dictionary and find the key of the best model and store its score.
+
