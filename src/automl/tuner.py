1b102213bd1c621db176787069ce90b140436901
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-08-07 16:54:27 +0300

    Add notebook 2 that explains the hyperparameter tuning

---
+++ src/automl/tuner.py
@@ -0,0 +1,15 @@
+# without this file the system will use the default values for the parameters when using the models. 
+# Instead AutoML should automatically search for better values (hyperparameter combination).
+
+
+# See notebook_2.ipynb to understand automatic hyperparameter combination.
+
+# What the tuner should do: 
+
+# Example:
+# Input: RandomForestRegressor
+# Output: RandomForestRegressor(n_estimators = 350, max_depth = 18, min_samples_split = 4)
+
+
+
+
